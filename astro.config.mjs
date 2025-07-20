f02eb0c992e433d2b51ac08aa62b3cc5e4d24c69
zmsaddi <35869249+zmsaddi@users.noreply.github.com>
2025-07-20 02:42:52 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://cholloberri.eus',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'eu', 'fr', 'en', 'nl', 'de'],
